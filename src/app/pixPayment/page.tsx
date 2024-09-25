4593cde68e6c5200d4b8025b1fda9eed27c2ff1f
Raphael Rangel <rapha.carvalho7@gmail.com>
2024-09-25 11:46:42 -0300

    commit

--- src/app/pixPayment/page.tsx
+++ src/app/pixPayment/page.tsx
@@ -2,34 +2,44 @@
 import { PixPaymentMain } from "@/components/pix-payment-main";
 import { useValue } from "@/hooks/useValue";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import dynamic from "next/dynamic";
 import { DefaultLayout } from "@/components/default-layout";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
-import { UserInfoTypes } from "@/types/user-info";
 
 
 
 const PixPayment=({searchParams}: {searchParams:{idBuy:string}})=>{
-  const {setUserInfo, userInfo}= useValue();
+  const [loading, setLoading] =useState(true);
+  const {setUserInfo}= useValue();
   const router = useRouter();
   useEffect(()=>{ 
     getValues();
   },[])
-  
   const getValues = async()=>{
+    setLoading(true)
     const response = await axios.get(`https://json-server-woovi-db.vercel.app/users?identificatorBuy=${searchParams.idBuy}`);
-    await setUserInfo(response.data[0]);
+    console.log(response.data)
+    if(response.data[0].paidPix){
+      router.push(`/pixCreditCard?idBuy=${response.data[0].identificatorBuy}`)
+    } else{
+      setUserInfo(response.data[0]);
+    }
+    setLoading(false)
   }
-
   return (
-    <DefaultLayout>
-      <Header/>
-      <PixPaymentMain/>
-      <Footer/>
-    </DefaultLayout>
+    <>
+    {!loading ? 
+      <DefaultLayout>
+        <Header/>
+        <PixPaymentMain/>
+        <Footer/>
+      </DefaultLayout>
+      : null
+    }
+    </>
   )
 }
 
